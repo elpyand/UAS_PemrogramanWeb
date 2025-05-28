2f72be6c1af555fd25427c406a54fc546bbacd34
Piki Alpian <pikialpian5@gmail.com>
2025-05-28 14:31:47 +0700

    Update laporin.js

--- routes/laporin.js
+++ routes/laporin.js
@@ -3,13 +3,23 @@ const router = express.Router();
 const multer = require('multer');
 const path = require('path');
 const mysql = require('mysql2/promise');
+require('dotenv').config();
 
 // Koneksi Database
-const db = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'db_laporan'
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('❌ Gagal koneksi DB:', err.message);
+  } else {
+    console.log('✅ Terkoneksi ke Railway MySQL!');
+  }
 });
 
 // Setup multer untuk upload file
